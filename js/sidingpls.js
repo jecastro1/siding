e5201e6fb92e5de9caaaeb5a8beb78c5c4b1447b
Jaime Castro <jecastro.ie@gmail.com>
2018-08-12 22:10:56 -0300

    Formatted code

--- js/sidingpls.js
+++ js/sidingpls.js
@@ -1,13 +1,13 @@
-function setReferer(){
-  document.referer = "https://www.ing.uc.cl";	
+function setReferer() {
+  document.referer = "https://www.ing.uc.cl";
 }
 
-function randomBG(){
-  var h = Math.floor(360*Math.random());
-  var s = 50+20*Math.random();
-  var l = 30+20*Math.random();
+function randomBG() {
+  var h = Math.floor(360 * Math.random());
+  var s = 50 + 20 * Math.random();
+  var l = 30 + 20 * Math.random();
 
-  var col = 'hsl('+h+','+s+'%,'+l+'%)';
+  var col = 'hsl(' + h + ',' + s + '%,' + l + '%)';
   document.body.style.background = col;
 }
 
@@ -23,42 +23,42 @@ function runTask(task, frequencyInMilliseconds) {
   stopTimer();
 
   task();
-  currentTimer = setInterval(function() {
+  currentTimer = setInterval(function () {
     task();
   }, taskTime);
 }
 
-function getHomepageIRI(){
-	var d = new Date();
-	var m = d.getMonth();
-	var y = d.getFullYear();
-	// too easy, don't read
-	if (!m) {
-		y--;
-		m=1<<4;
-	}
-	var s = Math.ceil(m / 6);
-	return "https://intrawww.ing.puc.cl/siding/dirdes/ingcursos/cursos/index.phtml?per_lista_cursos=2" + s + "_" + y + "&acc_inicio=mis_cursos";
+function getHomepageIRI() {
+  var d = new Date();
+  var m = d.getMonth();
+  var y = d.getFullYear();
+  // too easy, don't read
+  if (!m) {
+    y--;
+    m = 1 << 4;
+  }
+  var s = Math.ceil(m / 6);
+  return "https://intrawww.ing.puc.cl/siding/dirdes/ingcursos/cursos/index.phtml?per_lista_cursos=2" + s + "_" + y + "&acc_inicio=mis_cursos";
 }
 
-function registerListeners(){
+function registerListeners() {
   $form = $('form[id=LoginForm]');
   $usr = $('input[id=login]');
   $pwd = $('input[id=passwd]');
 
-  $form.submit(function(e) {
+  $form.submit(function (e) {
     usr_l = $usr.val().length;
     pwd_l = $pwd.val().length;
 
 
-    if (usr_l<1) {
+    if (usr_l < 1) {
       alert('derp, usr pls');
     }
-    else if (pwd_l<1) {
+    else if (pwd_l < 1) {
       alert('derp, pwd pls');
     }
     else {
-      $('iframe').bind('load', function() {
+      $('iframe').bind('load', function () {
         window.location.replace(getHomepageIRI());
       });
 
@@ -70,19 +70,16 @@ function registerListeners(){
 
   });
 
-  $('body').keydown(function(event) {
-    if(Math.random()>0.4)
+  $('body').keydown(function (event) {
+    if (Math.random() > 0.4)
       randomBG();
   });
 }
 
-
-
-
-document.addEventListener('page:load', function(){
+document.addEventListener('page:load', function () {
   registerListeners();
 });
-$(document).ready(function(){
+$(document).ready(function () {
   setReferer();
   randomBG();
   registerListeners();
